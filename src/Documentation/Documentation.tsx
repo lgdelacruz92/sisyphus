949e807e62b280b162c81928809b36c4a7c1dca3
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-21 16:15:36 -0400

    added documentation

--- src/Documentation/Documentation.tsx
+++ src/Documentation/Documentation.tsx
@@ -1,9 +1,17 @@
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 interface DocumentationProps {}
 
 const Documentation: React.FC<DocumentationProps> = () => {
-  return <div>Documentation</div>;
+  return (
+    <Center>
+      <Box maxW="70%">
+        <Heading>Documentation</Heading>
+        <br></br>
+      </Box>
+    </Center>
+  );
 };
 
 export default Documentation;
